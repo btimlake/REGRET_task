b6953a90fe67cbe4fec16d03c8ae39a636866dbb
btimlake <ben@bentimberlake.com>
2016-04-06 18:10:36 +0200

    Two wheels
    Made two wheels display with some variables applied to make it easier
    to modify the game trial to trial

--- regretTask.m
+++ regretTask.m
@@ -12,14 +12,15 @@ Screen('Preference', 'SkipSyncTests', 2);
 screens = Screen('Screens');
 
 % Draw to the external screen if avaliable
-screenNumber = max(screens);
+% screenNumber = max(screens);
+screenNumber = 0;
 
 % Define black and white
 white = WhiteIndex(screenNumber);
 black = BlackIndex(screenNumber);
 
 % Open an on screen window
-[window, windowRect] = PsychImaging('OpenWindow', screenNumber, black);
+[window, windowRect] = PsychImaging('OpenWindow', screenNumber, white);
 
 % Get the size of the on screen window
 [screenXpixels, screenYpixels] = Screen('WindowSize', window);
@@ -27,6 +28,26 @@ black = BlackIndex(screenNumber);
 % Get the centre coordinate of the window
 [xCenter, yCenter] = RectCenter(windowRect);
 
+%% Set position info
+wheelRadius = (screenXpixels*.13);
+
+% Set positions of graphical elements
+leftWheelpos = [screenXpixels*.25-wheelRadius screenYpixels*.5-wheelRadius screenXpixels*.25+wheelRadius screenYpixels*.5+wheelRadius];
+rightWheelpos = [screenXpixels*.75-wheelRadius screenYpixels*.5-wheelRadius screenXpixels*.75+wheelRadius screenYpixels*.5+wheelRadius];
+leftArrowpos = [screenXpixels*.25-wheelRadius*.25 screenYpixels*.5-wheelRadius*.75 screenXpixels*.25+wheelRadius*.25 screenYpixels*.5+wheelRadius*.75];
+rightArrowpos = [screenXpixels*.75-wheelRadius*.25 screenYpixels*.5-wheelRadius*.75 screenXpixels*.75+wheelRadius*.25 screenYpixels*.5+wheelRadius*.75];
+
+% temporary; will be modified to make these vary depending on choice
+locChoice = leftWheelpos;  
+locNonChoice = rightWheelpos; 
+arrowChoice = leftArrowpos;
+arrowNonChoice = rightArrowpos;
+
+% Set some variables
+NUMROUNDS = 3;
+lotteryOutcome = 0 + (1-0).*rand(NUMROUNDS,2); % Creates array of random outcome probabilities for both wheels in each round
+
+%% back to original
 % Maximum priority level
 topPriorityLevel = MaxPriority(window);
 Priority(topPriorityLevel);
@@ -53,19 +74,36 @@ vbl = Screen('Flip', window);
 waitframes = 1;
 
 % Animation loop
+for i=1:NUMROUNDS
     
-    lotteryOutcome = 0.33;      %%!! this is the outcome of the lottery's probability roll, a number between 0 and 1
+%     lotteryOutcome = 0.33;      %%!! this is the outcome of the lottery's probability roll, a number between 0 and 1
     time.start = GetSecs;
-    ang=0;
-    
-    while(ang < (4*360 + 360*lotteryOutcome)) %% 4*360 means the arrow will spin 4 full rounds before stopping at the outcome
-        ang=ang+degPerFrame;
-        Screen('DrawTexture', window, texProb66, [0 0 550 550], [xCenter-650/2 yCenter-650/2 xCenter+650/2 yCenter+650/2]); %%!! Location should not be hard coded
-        Screen('DrawTexture', window, texArrow, [0 0 96 960], [xCenter-24 yCenter-240 xCenter+48 yCenter+240],ang);
+    angChoice=0;
+    angNonChoice=0;
+%  && angNonChoice < (4*360 + 360*lotteryOutcome(i,2))
+    while( (angChoice < (4*360 + 360*lotteryOutcome(i,1))) || (angNonChoice < (4*360 + 360*lotteryOutcome(i,2))) ) %% 4*360 means the arrow will spin 4 full rounds before stopping at the outcome
+        angChoice=angChoice+degPerFrame;
+        angNonChoice=angNonChoice+degPerFrame;
+        % choice wheel & arrow
+        Screen('DrawTexture', window, texProb66, [0 0 550 550], locChoice); %%!! Location should not be hard coded
+        Screen('DrawTexture', window, texArrow, [0 0 96 960], arrowChoice,angChoice);
+        % non-choice wheel & arrow        
+        Screen('DrawTexture', window, texProb66, [0 0 550 550], locNonChoice); %%!! Location should not be hard coded
+        Screen('DrawTexture', window, texArrow, [0 0 96 960], arrowNonChoice,angNonChoice);
         vbl  = Screen('Flip', window, vbl + (waitframes - 0.5) * ifi);
     end
+    
+%     while(angNonChoice < (4*360 + 360*lotteryOutcome(i,2))) %% 4*360 means the arrow will spin 4 full rounds before stopping at the outcome
+%         angNonChoice=angNonChoice+degPerFrame;
+%         % non-choice wheel & arrow        
+%         Screen('DrawTexture', window, texProb66, [0 0 550 550], locNonChoice); %%!! Location should not be hard coded
+%         Screen('DrawTexture', window, texArrow, [0 0 96 960], arrowNonChoice,angNonChoice);
+%     end
+%         vbl  = Screen('Flip', window, vbl + (waitframes - 0.5) * ifi);
+
     WaitSecs(2); 
 
+end
 
 % Clear the screen
 sca;
